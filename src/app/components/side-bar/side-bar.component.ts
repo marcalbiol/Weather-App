30868cba2ac65f78a6fab003195d03c1ff3ad08e
danimacs <dani-mac@hotmail.es>
2023-03-12 01:31:39 +0100

    Cambiar nombre variable addressLine

--- src/app/components/side-bar/side-bar.component.ts
+++ src/app/components/side-bar/side-bar.component.ts
@@ -19,8 +19,8 @@ export class SideBarComponent implements OnInit {
   }
 
   onPlaceSelected(event: { properties: { address_line1: string; address_line2: string; lat: string; lon: string; }; }) {
-    let addressLine2 = event.properties.address_line2 != null ? event.properties.address_line2 : event.properties.address_line1;
-    let city = new City(addressLine2, event.properties.lat, event.properties.lon);
+    let addressLine = event.properties.address_line2 != null ? event.properties.address_line2 : event.properties.address_line1;
+    let city = new City(addressLine, event.properties.lat, event.properties.lon);
     this.onSelected.emit(city);
   }
 
